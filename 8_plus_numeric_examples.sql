40522c05210df688cf6036f265b63fa85e54d5c1
Brady Barker <brady.d.barker@gmail.com>
2023-06-04 17:02:50 -0400

    Completed 8 plus

--- 8_plus_numeric_examples.sql
+++ 8_plus_numeric_examples.sql
@@ -42,3 +42,27 @@ GROUP BY subject
 -- Show the percentage of students who A_STRONGLY_AGREE to question 22 for the subject 
 -- '(8) Computer Science' show the same figure for the subject '(H) Creative Arts and Design'.
 
+SELECT subject, ROUND(SUM(response * A_STRONGLY_AGREE)/SUM(response),0)
+  FROM nss
+ WHERE question='Q22'
+   AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design')
+GROUP BY subject
+
+-- Show the average scores for question 'Q22' for each institution that 
+-- include 'Manchester' in the name.
+
+SELECT institution, ROUND(SUM(score*response)/SUM(response))
+FROM nss
+WHERE question='Q22'
+AND (institution LIKE '%Manchester%')
+GROUP BY institution
+ORDER BY institution
+
+-- Show the institution, the total sample size and the number of computing students 
+-- for institutions in Manchester for 'Q01'.
+
+SELECT institution, SUM(sample), SUM(CASE WHEN subject='(8) Computer Science' THEN sample ELSE 0 END)
+  FROM nss
+  WHERE question = 'Q01'
+  AND (institution LIKE '%Manchester%')
+GROUP BY institution
